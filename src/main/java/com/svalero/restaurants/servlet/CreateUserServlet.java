3cb738f8a9a2a15a01ce54dc60324917e0e39610
Andrea Fernandez de la Rosa <a28707@svalero.com>
2025-05-16 18:32:39 +0200

    More servlets

---
+++ src/main/java/com/svalero/restaurants/servlet/CreateUserServlet.java
@@ -0,0 +1,44 @@
+package com.svalero.restaurants.servlet;
+
+import com.svalero.restaurants.dao.UserDao;
+import com.svalero.restaurants.database.Database;
+import com.svalero.restaurants.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.UUID;
+
+@WebServlet("/create_user")
+
+public class CreateUserServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            String idUser = UUID.randomUUID().toString().substring(0, 8);
+            String name = request.getParameter("name");
+            String surname = request.getParameter("surname");
+            int phone = Integer.parseInt(request.getParameter("phone"));
+            String email = request.getParameter("email");
+
+            User user = new User(idUser, name, surname, phone, email);
+
+            Database database = new Database();
+            database.connect();
+
+            UserDao userDao = new UserDao(database.getConnection());
+            userDao.addUser(user);
+
+            database.disconnect();
+
+            response.sendRedirect("list_users");
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            response.sendRedirect("error.jsp");
+        }
+    }
+}
