cc6fd89ed0cdbec26580ff31fb22e6cfe638461f
Andrea Fernandez de la Rosa <a28707@svalero.com>
2025-05-16 16:55:28 +0200

    Java classes

--- src/main/java/com/svalero/restaurants/model/User.java
+++ src/main/java/com/svalero/restaurants/model/User.java
@@ -1,4 +1,12 @@
 package com.svalero.restaurants.model;
 
+import lombok.Data;
+
+@Data
 public class User {
+    private String idUser;
+    private String name;
+    private String surname;
+    private int phone;
+    private String email;
 }
