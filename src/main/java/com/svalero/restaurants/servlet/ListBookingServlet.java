7a837bb962b44ec883755edf93a0da3b289d681b
Andrea Fernandez de la Rosa <a28707@svalero.com>
2025-05-16 17:59:52 +0200

    Servlets

--- src/main/java/com/svalero/restaurants/servlet/ListBookingServlet.java
+++ src/main/java/com/svalero/restaurants/servlet/ListBookingServlet.java
@@ -1,30 +1,33 @@
 package com.svalero.restaurants.servlet;
 
+
 import com.svalero.restaurants.dao.BookingDao;
 import com.svalero.restaurants.database.Database;
+import com.svalero.restaurants.model.Booking;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
-@WebServlet("/delete_booking")
-public class DeleteBookingServlet extends HttpServlet {
+@WebServlet("/list_bookings")
+
+public class ListBookingServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String idBooking = request.getParameter("id");
-
         try {
             Database database = new Database();
             database.connect();
 
             BookingDao bookingDao = new BookingDao(database.getConnection());
-            bookingDao.deleteBooking(idBooking);
+            List<Booking> bookings = bookingDao.getAllBookings();
+
+            request.setAttribute("bookings", bookings);
+            request.getRequestDispatcher("list_bookings.jsp").forward(request, response);
 
             database.disconnect();
-
-            response.sendRedirect("list_bookings");
 
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
